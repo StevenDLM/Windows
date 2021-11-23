9e09fc7b0711714f43506746a7d837039e8a6820
StevenDeLaMata <94876368+StevenDeLaMata@users.noreply.github.com>
2021-11-23 15:46:07 +1100

    Create Unifi-Adopt.ps1

---
+++ Unifi-Adopt.ps1
@@ -0,0 +1,14 @@
+Params(
+[string]$IPadress = "x.x.x.x",
+[string]$AdoptURL = "http://unifi.citychic.com.au:8081/inform",
+[string]$SSHpass)
+
+$Creds = New-Object System.Management.Automation.PSCredential ('admin', $(ConvertTo-SecureString $SSHpass -AsPlainText -Force))
+	
+		New-SSHSession -ComputerName $IPadress -Credential $Creds -Force | out-null
+		$session = Get-SSHSession -Index 0
+		$stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
+		$stream.Write("set-inform $AdoptURL`n")
+		start-sleep -s 2
+		$stream.Write("set-inform $AdoptURL`n")
+		Remove-SSHSession 0 | out-null
